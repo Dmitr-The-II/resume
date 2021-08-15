f788a2ea6ad0ce0b7cf792c12d700e3f9bf88247
master <dkim178@myseneca.ca>
2021-08-15 04:10:03 +0300

    js done

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,17 @@ function postalFilter (postalCode) {
     }
     return null;
 }
+function getValue(radio) {
+    debugger;
+    if (radio.value == "Hiring") {
+      document.querySelector("#trHourlyRate").style.visibility = "visible";
+    }
+  }
+  function hideValue(val) {
+    if (val.value == "Question") {
+      document.querySelector("#trHourlyRate").style.visibility = "hidden";
+    }
+    if (val.value == "Comment") {
+      document.querySelector("#trHourlyRate").style.visibility = "hidden";
+    }
+  }
